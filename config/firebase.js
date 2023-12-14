b78955288c42c1c0c578ffea7d9ce4a352fdf369
Eduardosent <50090832+Eduardosent@users.noreply.github.com>
2023-12-14 18:48:33 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCsYw21fw1Pqu0lvJ5msXYmYhUbrc8bchs",
-  authDomain: "chatapp-c7ef7.firebaseapp.com",
-  projectId: "chatapp-c7ef7",
-  storageBucket: "chatapp-c7ef7.appspot.com" ,
-  messagingSenderId: "907262581888",
-  appId: "1:907262581888:web:f48fab9804b95f32b49a65"
+  apiKey: "your keys",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "" ,
+  messagingSenderId: "9",
+  appId: ""
 };
 
 // Initialize Firebase
